bbd397bca14b53e26ff8f2426ae4ca7589c8c66e
shameem1992 <shameem336699@gmail.com>
2023-05-25 15:45:06 +0530

    EmployeeTest

---
+++ src/main/java/Employee.java
@@ -0,0 +1,13 @@
+
+
+public class Employee {
+	private String name;
+
+	public Employee(String name) {
+		this.name = name;
+	}
+	public void displayname() {
+		System.out.println("welcome"+this.name);
+	}
+
+}
---
+++ src/main/java/TestEmp.java
@@ -0,0 +1,21 @@
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+public class TestEmp {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("employeeContext.xml");
+		
+		Employee stu = (Employee)context.getBean("empbean");
+		stu. displayname();
+		 
+	    }
+		
+
+	}
+
+
+
